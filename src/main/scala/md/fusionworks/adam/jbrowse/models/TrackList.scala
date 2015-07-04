d5f4e709568cc61b6bff34f457a22251400ccc4f
Sergiu354 <Sergiu3540@gmail.com>
2015-07-04 21:40:37 +0300

    Modified class "Features, Feature, SubFeatures"

--- src/main/scala/md/fusionworks/adam/jbrowse/models/TrackList.scala
+++ src/main/scala/md/fusionworks/adam/jbrowse/models/TrackList.scala
@@ -15,18 +15,18 @@ object JsonProtocol extends DefaultJsonProtocol {
 object JbrowseUtil {
   def getTrackList = {
     TrackList(tracks = List(Track("mygene_track",
-    "Genes",
-    "JBrowse/View/Track/HTMLFeatures",
-    "JBrowse/Store/SeqFeature/REST",
-    "http://my.site.com/rest/api/base",
-    List(Query("tyrannosaurus", Some("gene")))
-  ), Track("my_sequence_track",
-    "DNA",
-    "JBrowse/View/Track/Sequence",
-    "JBrowse/Store/SeqFeature/REST",
-    "http://my.site.com/rest/api/base",
-    List(Query("tyrannosaurus", sequence = Some(true))))))
-    }
+      "Genes",
+      "JBrowse/View/Track/HTMLFeatures",
+      "JBrowse/Store/SeqFeature/REST",
+      "http://my.site.com/rest/api/base",
+      List(Query("tyrannosaurus", Some("gene")))
+    ), Track("my_sequence_track",
+      "DNA",
+      "JBrowse/View/Track/Sequence",
+      "JBrowse/Store/SeqFeature/REST",
+      "http://my.site.com/rest/api/base",
+      List(Query("tyrannosaurus", sequence = Some(true))))))
+  }
   def getRefSeqs={
     RefSeqs("chr1",0,12345678)
   }
@@ -44,186 +44,178 @@ object JbrowseUtil {
   }
 
   def getFeatures={
-    Features(List(Feature(123,456),
-                  Feature2(123,456,42),
-                  Feature3("seq",0,14,42),
-                  Feature4(
-                    "mRNA",
-                    "gattacagattaca",
-                    5975,
-                    9744,
-                    0.84,
-                    1,
-                    "au9.g1002.t1",
-                    "globallyUniqueString3",
-                    List(
-                      SubFeature(
-                        "five_prime_UTR",
-                        5975,
-                        6109,
-                        Some(0.98),
-                        1,
-                        None
-                      ),
-                      SubFeature(
-                      "start_codon",
-                        6110,
-                        6112,
-                        None,
-                        1,
-                        Some(0)
-                      ),
-                      SubFeature(
-                        "CDS",
-                        6110,
-                        6148,
-                        Some(1),
-                        1,
-                        Some(0)
-                      ),
-                      SubFeature(
-                        "CDS",
-                        6615,
-                        6683,
-                        Some(1),
-                        1,
-                        Some(0)
-                      ),
-                      SubFeature(
-                        "CDS",
-                        6758,
-                        7040,
-                        Some(1),
-                        1,
-                        Some(0)
-                      ),
-                      SubFeature(
-                        "CDS",
-                        7142,
-                        7319,
-                        Some(1),
-                        1,
-                        Some(2)
-                      ),
-                      SubFeature(
-                        "CDS",
-                        7411,
-                        7686,
-                        Some(1),
-                        1,
-                        Some(1)
-                      ),
-                      SubFeature(
-                        "CDS",
-                        7748,
-                        7850,
-                        Some(1),
-                        1,
-                        Some(0)
-                      ),
-                      SubFeature(
-                        "CDS",
-                        7953,
-                        8098,
-                        Some(1),
-                        1,
-                        Some(2)
-                      ),
-                      SubFeature(
-                        "CDS",
-                        8166,
-                        8320,
-                        Some(1),
-                        1,
-                        Some(0)
-                      ),
-                      SubFeature(
-                        "CDS",
-                        8419,
-                        8614,
-                        Some(1),
-                        1,
-                        Some(1)
-                      ),
-                      SubFeature(
-                        "CDS",
-                        8708,
-                        8811,
-                        Some(1),
-                        1,
-                        Some(0)
-                      ),
-                      SubFeature(
-                        "CDS",
-                        8927,
-                        9273,
-                        Some(1),
-                        1,
-                        Some(1)
-                      ),
-                      SubFeature(
-                        "CDS",
-                        9414,
-                        9494,
-                        Some(1),
-                        1,
-                        Some(0)
-                      ),
-                      SubFeature(
-                        "stop_codon",
-                        9492,
-                        9494,
-                        None,
-                        1,
-                        Some(0)
-                      ),
-                      SubFeature(
-                        "three_prime_UTR",
-                        9495,
-                        9744,
-                        Some(0.86),
-                        1,
-                        None
-                      )
-                    )
-                  )
-    )
+    Features(features = List(
+      Feature(None, None, 123, 456, None, None, None, None, None),
+      Feature(None, None, 123, 456, Some(42),None, None, None, None),
+      Feature(Some("gattacagattaca"),None,0,14,None,None,None,None),
+      Feature(None, Some("mRNA"),5975,9744,Some(0.84),Some(1),Some("au9.g1002.t1"),Some("globallyUniqueString3"),
+        subfeatures = Some(List(SubFeatures(
+          "five_prime_UTR",
+          5975,
+          6109,
+          Some(0.98),
+          1,
+          None
+        ),
+          SubFeatures(
+            "start_codon",
+            6110,
+            6112,
+            None,
+            1,
+            Some(0)
+          ),
+          SubFeatures(
+            "CDS",
+            6110,
+            6148,
+            Some(1),
+            1,
+            Some(0)
+          ),
+          SubFeatures(
+            "CDS",
+            6615,
+            6683,
+            Some(1),
+            1,
+            Some(0)
+          ),
+          SubFeatures(
+            "CDS",
+            6758,
+            7040,
+            Some(1),
+            1,
+            Some(0)
+          ),
+          SubFeatures(
+            "CDS",
+            7142,
+            7319,
+            Some(1),
+            1,
+            Some(2)
+          ),
+          SubFeatures(
+            "CDS",
+            7411,
+            7686,
+            Some(1),
+            1,
+            Some(1)
+          ),
+          SubFeatures(
+            "CDS",
+            7748,
+            7850,
+            Some(1),
+            1,
+            Some(0)
+          ),
+          SubFeatures(
+            "CDS",
+            7953,
+            8098,
+            Some(1),
+            1,
+            Some(2)
+          ),
+          SubFeatures(
+            "CDS",
+            8166,
+            8320,
+            Some(1),
+            1,
+            Some(0)
+          ),
+          SubFeatures(
+            "CDS",
+            8419,
+            8614,
+            Some(1),
+            1,
+            Some(1)
+          ),
+          SubFeatures(
+            "CDS",
+            8708,
+            8811,
+            Some(1),
+            1,
+            Some(0)
+          ),
+          SubFeatures(
+            "CDS",
+            8927,
+            9273,
+            Some(1),
+            1,
+            Some(1)
+          ),
+          SubFeatures(
+            "CDS",
+            9414,
+            9494,
+            Some(1),
+            1,
+            Some(0)
+          ),
+          SubFeatures(
+            "stop_codon",
+            9492,
+            9494,
+            None,
+            1,
+            Some(0)
+          ),
+          SubFeatures(
+            "three_prime_UTR",
+            9495,
+            9744,
+            Some(0.86),
+            1,
+            None
+          )
+        )
+        )
+      ))
     )
   }
+
 }
 
 case class TrackList(tracks: List[Track]
                       )
 
 case class Track(
-                   label: String,
-                   key: String,
-                   `type`: String,
-                   storeClass: String,
-                   baseUrl: String,
-                   query: List[Query]
-                   )
-
+                  label: String,
+                  key: String,
+                  `type`: String,
+                  storeClass: String,
+                  baseUrl: String,
+                  query: List[Query]
+                  )
 
 case class Query(
-                   organism: String,
-                   soType: Option[String] = None,
-                   sequence: Option[Boolean] = None
-                   )
+                  organism: String,
+                  soType: Option[String] = None,
+                  sequence: Option[Boolean] = None
+                  )
 
 case class RefSeqs(
-                  name:String,
-                  start:Int,
-                  end:Int
+                    name:String,
+                    start:Int,
+                    end:Int
                     )
 
 
 case class TracksConf(
-                     storeClass: String,
-                     urlTemplate: String,
-                     category: String,
-                     `type`: String,
-                     key: String
+                       storeClass: String,
+                       urlTemplate: String,
+                       category: String,
+                       `type`: String,
+                       key: String
                        )
 
 case class Global(
@@ -239,44 +231,24 @@ case class Features(
                      features: List[Feature]
                      )
 
-
 case class Feature(
-                    start:Int,
-                    end: Int
+                    seq: Option[String]=None,
+                    `type`: Option[String]=None,
+                    start: Int,
+                    end: Int,
+                    score: Option[Double]=None,
+                    strand: Option[Int]=None,
+                    name: Option[String]=None,
+                    uniqueID: Option[String]=None,
+                    subfeatures: Option[List[SubFeatures]]=None
                     )
 
-case class Feature2(
-                     override val start: Int,
-                     override val end: Int,
-                     score: Double
-                     ) extends Feature(start,end)
-
-
-
-case class Feature3(
-                    seq: String,
-                    override val start: Int,
-                    override val end: Int,
-                    override val score: Double
-                      ) extends Feature2(start,end,score)
-
-case class Feature4(
-                   `type`: String,
-                   override val seq: String,
-                   override val start: Int,
-                   override val end: Int,
-                   override val score: Double,
-                   strand: Int,
-                   name: String,
-                   uniqueID: String,
-                   subfeatures:List[SubFeature]
-                     ) extends Feature3(seq,start,end,score)
-
-case class SubFeature(
-                      `type`: String,
-                      start: Int,
-                      end: Int,
-                      score: Option[Double]=None,
-                      strand: Int,
-                      phase: Option[Int]=None
+case class SubFeatures(
+                        `type`: String,
+                        start: Int,
+                        end: Int,
+                        score: Option[Double]=None,
+                        strand: Int,
+                        phase: Option[Int]=None
                         )
+
