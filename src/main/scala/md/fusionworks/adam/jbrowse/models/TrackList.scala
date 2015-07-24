82a7252ebd39f019aaad1ea58d42a31a8807afaa
sergiu <sergiu3540@gmail.com>
2015-07-24 17:43:10 +0300

    Add to Feature class and function: "flag"

--- src/main/scala/md/fusionworks/adam/jbrowse/models/TrackList.scala
+++ src/main/scala/md/fusionworks/adam/jbrowse/models/TrackList.scala
@@ -1,7 +1,12 @@
 package md.fusionworks.adam.jbrowse.models
 
+import com.typesafe.config.ConfigFactory
+import htsjdk.samtools.{SAMFileHeader, SAMRecord}
 import org.apache.spark.sql.SQLContext
 import org.apache.spark.{SparkConf, SparkContext}
+import org.bdgenomics.adam.converters.AlignmentRecordConverter
+import org.bdgenomics.adam.rdd.ADAMContext
+import org.bdgenomics.adam.rdd.read.AlignmentRecordRDDFunctions
 import spray.json.DefaultJsonProtocol
 
 
@@ -11,7 +16,7 @@ object JsonProtocol extends DefaultJsonProtocol {
   implicit val trakListFormat = jsonFormat1(TrackList)
   implicit val refSeqsFormat = jsonFormat6(RefSeqs)
   implicit val globalFormat = jsonFormat6(Global)
-  implicit val featureFormat = jsonFormat9(Feature)
+  implicit val featureFormat = jsonFormat10(Feature)
   implicit val featuresFormat = jsonFormat1(Features)
 }
 
@@ -19,7 +24,11 @@ object JbrowseUtil {
   val conf = new SparkConf().setAppName("Simple Application").setMaster("local[2]")
   val sc = new SparkContext(conf)
   val sqc = new SQLContext(sc)
-  val tableSQL = sqc.read.parquet("adamtest.adam")
+  val ac = new ADAMContext(sc)
+  val readAC = ac.loadAlignments("adamtest.adam")
+  val adamPath = ConfigFactory.load().getString("adam.path")
+  val tableSQL = sqc.read.parquet(adamPath)
+  val df = sqc.read.parquet(adamPath)
   tableSQL.registerTempTable("ADaM_Table")
   val end = sqc.sql("SELECT MAX(`end`) FROM ADaM_Table").collect().apply(0).getLong(0)
   val start = sqc.sql("SELECT MIN(start) FROM ADaM_Table").collect().apply(0).getLong(0)
@@ -53,10 +62,26 @@ object JbrowseUtil {
 
   def getFeatures(getFactStat: Long, getFactEnd: Long) = {
 
-    val regist = sqc.sql(s"SELECT readName, sequence, start, `end`, cigar, mapq, mateAlignmentStart, qual FROM ADaM_Table WHERE start >= $getFactStat AND start <= $getFactEnd ORDER BY start ASC ")
+    val filt = readAC.filter(x=>(x.getStart!=null && x.getStart>=getFactStat && x.getStart<=getFactEnd))
+    val al=new AlignmentRecordRDDFunctions(filt)
+    val flags = al.adamConvertToSAM()._1.map(x=> ( x.get().getFlags)).collect.toList
+    val itfl = flags
 
-    val sampledata= regist.collect().map(x => Feature(x.getString(0),x.getString(1),x.getLong(2),x.getLong(3),x.getString(4),x.getInt(5),x.getLong(6),x.getString(0)+"_"+x.getLong(2),x.getString(7).map(_-33).mkString(" "))).toList
-    Features(features = sampledata)
+    val dfreg=df.select(
+      df("readName"),df("sequence"),df("start"),df("`end`"),df("cigar"),df("mapq"),df("mateAlignmentStart"),df("qual")
+    ).filter(df("start")>=getFactStat && df("start")!=null).filter(df("start")<=getFactEnd && df("start")!=null).orderBy("start")
+    val sampledf=dfreg.collect.map(x=>Feature(x.getString(0),x.getString(1),x.getLong(2),x.getLong(3),x.getString(4),x.getInt(5),x.getLong(6),x.getString(0)+"_"+x.getLong(2),x.getString(7).map(_-33).mkString(" "),itfl )).toList
+    Features(features = sampledf)
+
+
+
+/*
+        val regist = sqc.sql(s"SELECT readName, sequence, start, `end`, cigar, mapq, mateAlignmentStart, qual FROM ADaM_Table WHERE start >= $getFactStat AND start <= $getFactEnd ORDER BY start ASC ")
+
+        val sampledata= regist.collect().map(x => Feature(x.getString(0),x.getString(1),x.getLong(2),x.getLong(3),x.getString(4),x.getInt(5),x.getLong(6),x.getString(0)+"_"+x.getLong(2),x.getString(7).map(_-33).mkString(" "))).toList
+        Features(features = sampledata)
+*/
+
 
   }
 
@@ -116,6 +141,7 @@ case class Feature(
                     map_qual: Int,
                     mate_start: Long,
                     uniqueID: String,
-                    qual: String
+                    qual: String,
+                    flag: List[Int]
                     )
 
