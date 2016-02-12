695782ccde2cf246d9a85342dbc6982da6e31bad
Anton Stamov <antonstamov@gmail.com>
2016-02-12 12:10:01 +0200

    refactor AdamConverter

--- src/main/scala/md/fusionworks/adam/jbrowse/tools/AdamConverter.scala
+++ src/main/scala/md/fusionworks/adam/jbrowse/tools/AdamConverter.scala
@@ -1,32 +1,21 @@
 package md.fusionworks.adam.jbrowse.tools
 
 import md.fusionworks.adam.jbrowse.spark.SparkContextFactory
-import org.bdgenomics.adam.rdd.{ADAMContext, ADAMRDDFunctions}
+import org.bdgenomics.adam.rdd.ADAMContext._
 
 object AdamConverter {
+  private val sc = SparkContextFactory.getSparkContext
 
-  def fastaToADAM(inputPath :String, outputPath :String){
-    val sc = SparkContextFactory.getSparkContext
-    val ac = new ADAMContext(sc)
-    val reads = ac.loadSequence(inputPath)
-    val save = new ADAMRDDFunctions(reads)
-    save.adamParquetSave(outputPath)
+  def fastaToADAM(inputPath: String, outputPath: String) = {
+    sc.loadSequence(inputPath).adamParquetSave(outputPath)
   }
 
-  def vcfToADAM(inputPath :String, outputPath :String){
-    val sc = SparkContextFactory.getSparkContext
-    val ac = new ADAMContext(sc)
-    val reads = ac.loadVariants(inputPath)
-    val save = new ADAMRDDFunctions(reads)
-    save.adamParquetSave(outputPath)
+  def vcfToADAM(inputPath: String, outputPath: String) = {
+    sc.loadVariants(inputPath).adamParquetSave(outputPath)
   }
 
-  def bam_samToADAM(inputPath :String, outputPath :String){
-    val sc = SparkContextFactory.getSparkContext
-    val ac = new ADAMContext(sc)
-      val reads = ac.loadAlignments(inputPath)
-      val save = new ADAMRDDFunctions(reads)
-      save.adamParquetSave(outputPath)
+  def bam_samToADAM(inputPath: String, outputPath: String) = {
+    sc.loadAlignments(inputPath).adamParquetSave(outputPath)
   }
 
 }
